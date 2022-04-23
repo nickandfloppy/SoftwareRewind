ff38fa9f9810a8d05566015c7f71ed0ef1111755
Frankie B <floppydisk05@aol.com>
2022-04-23 01:25:35 +0000

    Add navbar and update index

---
+++ index.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+	<link rel="stylesheet" href="/src/style.css">
+	<title>SoftwareRewind - Coming Soon</title>
+</head>
+
+<body>
+	<a href="/"><table><tr>
+		<td><img src="/favicon.png" width="50"></td>
+		<td><h1 style="margin: 0">Software<i>Rewind</i></h1><i>Archiving Software Since April 2022</i></td>
+	</tr></table></a>
+	<hr>
+    <?php include 'nav.html'; ?>
+    <hr>
+	<br>
+	<i>Coming Soon&trade;!</i>
+	<br>
+	<p>While you're here, check out <a href="http://drivers.nickandfloppy.com/">NetDrivers</a>, SoftwareRewind's sister project, focused on driver archival!</p>
+	<hr>
+	<address>&copy; 2022 <a href="http://nickandfloppy.com/">nick and floppy</a></address>
+</body>
+</html>
