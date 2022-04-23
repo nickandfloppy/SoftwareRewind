7242cf6f3038f2bffd8449508a3cc6d2b49d34a0
Frankie B <floppydisk05@aol.com>
2022-04-23 00:45:30 +0000

    Add base files

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+	<link rel="stylesheet" href="/src/style.css">
+	<title>SoftwareRewind - Coming Soon</title>
+</head>
+
+<body>
+	<a href="/"><table><tr>
+		<td><img src="/favicon.png" width="40"></td>
+		<td><h1 style="margin: 0">Software<i>Rewind</i></h1></td>
+	</tr></table></a>
+	<hr>
+	<i>Coming Soon&trade;</i>
+	<hr>
+	<address>&copy; 2022 <a href="http://nickandfloppy.com/">nick and floppy</a></address>
+</body>
+</html>
